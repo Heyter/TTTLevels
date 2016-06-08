0eef776436c15d3cbe70422d30b14eb5a7abe1e2
end360 <evanorr1999@gmail.com>
2016-06-08 13:55:32 -0400

    Update cl_init.lua

--- lua/levels/cl_init.lua
+++ lua/levels/cl_init.lua
@@ -10,11 +10,11 @@ local levelColors = {
 	Color(0, 141, 255)
 }
 
-net.Receive("PHXP_GetXP", function(len)
+net.Receive("TTTXP_GetXP", function(len)
 		myXP = net.ReadInt(32)
 		myLevel = net.ReadInt(32)
 end)
-net.Receive("PHXP_AddXP", function(len)
+net.Receive("TTTXP_AddXP", function(len)
 		local toadd = net.ReadInt(16)
 		addXP(toadd)
 end)
@@ -49,7 +49,7 @@ local function drawAddXP()
 		addxps = {}
 	end
 end
-hook.Add("HUDPaint", "PHXP_PaintXPUpdate", drawAddXP)
+hook.Add("HUDPaint", "TTTXP_PaintXPUpdate", drawAddXP)
 local twenty = ScrH()*.019
 local twentyfive = ScrH()*.023
 local ten = ScrH()*.0093
@@ -58,4 +58,4 @@ local function HUDPaint()
 	draw.RoundedBox(0, twentyfive, twentyfive, w*math.Clamp(myXP/xpForLevel,0,.98), h-ten, levelColors[(myLevel)%(#levelColors-1)] or Color(0,191,255))
 	draw.SimpleText("Level: "..myLevel,"Trebuchet24",(w+twentyfive)/2,h,color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
 end
-hook.Add("HUDPaint", "PHXP_PaintBar", HUDPaint)
+hook.Add("HUDPaint", "TTTXP_PaintBar", HUDPaint)
